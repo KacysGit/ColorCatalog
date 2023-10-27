68dca6440b4640fb7aaa5b33af8a748d8b90c4ef
Kacy <yot5@txstate.edu>
2023-10-27 01:44:25 -0500

    added button and buffer

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import {Text, View} from "react-native";
+import {ActivityIndicator, Button} from "react-native"
+// activity indicator creates a buffer indicator 
 
-export default function App() {
+export default function App(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <View style={{padding:50}}>
+      <Text> Hello World </Text>
+      <Text> Red </Text>
+      <Text> Blue </Text>
+      <Button title="Click Me" />
+      <ActivityIndicator size="large" color="61DBFB" />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    </View>
+  )
+}
