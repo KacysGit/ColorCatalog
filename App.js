1ffb7c96400f1b7cb82737f05dd0b8f9ab839676
Kacy <yot5@txstate.edu>
2023-10-27 13:22:15 -0500

    added explanations document

--- App.js
+++ App.js
@@ -19,19 +19,25 @@ export default function App(){
 const styles = StyleSheet.create({
   // add margin at the top of the view
   page: {
+    flexDirection: "column",
+    justifyContent: "space-around", // spaces the elements within a container
+    alignItems: "flex-start",
     marginTop: 40,
     backgroundColor: "#DDD",
     margin: 10, 
     padding: 5
   },
   text: {
+    //flex: 1, // evenly distribute box across whole row
     fontSize: 22,
+    textAlign: "center",
     color: "blue",
     backgroundColor: "grey",
     margin: 10, // the area around each text element
     padding: 5 // space to the inside of each text element
   },
   selectedText: {
+    alignSelf: "flex-end",
     backgroundColor: "white",
     color: "black"
   }
