971efd637bb7879a6da3f061cc3aff257da22813
Kacy <yot5@txstate.edu>
2023-10-27 02:37:52 -0500

    added styling

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
-import React from "react"
-import {Text, View} from "react-native";
-import {ActivityIndicator, Button} from "react-native"
-// activity indicator creates a buffer indicator 
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View
+} from "react-native";
 
 export default function App(){
   return (
-    <View style={{padding:50}}>
-      <Text> Hello World </Text>
-      <Text> Red </Text>
-      <Text> Blue </Text>
-      <Button title="Click Me" />
-      <ActivityIndicator size="large" color="61DBFB" />
-
+    <View style={styles.page}>
+      <Text style={styles.text}> Red </Text>
+      <Text style={[styles.text, styles.selectedText]}> Green </Text>
+      <Text style={styles.text}> Blue </Text>
     </View>
-  )
+  );
 }
+
+// add styles
+const styles = StyleSheet.create({
+  // add margin at the top of the view
+  page: {
+    marginTop: 40,
+    backgroundColor: "#DDD",
+    margin: 10, 
+    padding: 5
+  },
+  text: {
+    fontSize: 22,
+    color: "blue",
+    backgroundColor: "grey",
+    margin: 10, // the area around each text element
+    padding: 5 // space to the inside of each text element
+  },
+  selectedText: {
+    backgroundColor: "white",
+    color: "black"
+  }
+})
