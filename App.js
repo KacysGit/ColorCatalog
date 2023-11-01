7c959dcba718bcd9579dc100fdd51c58b19ec80b
Kacy <yot5@txstate.edu>
2023-11-01 00:14:03 -0500

    the user can now add colors

--- App.js
+++ App.js
@@ -1,23 +1,32 @@
 import React, { useState } from "react";
 import {
   FlatList, // scrollable list of content
-  StyleSheet,
-  Alert
+  StyleSheet
 } from "react-native";
 import ColorButton from "./Components/ColorButton";
-import defaultColors from "./data/defaultColors.json"
 import ColorForm from "./Components/ColorForm"
+import {generate} from "shortid";
+
+const useColors = () => {
+  const [colors, setColors] = useState([]);
+  const addColor = color => {
+    const newColor = {id: generate(), color}
+    setColors([newColor, ...colors]); // ... takes all the colors in the array and adds them to the end of the new array
+  };
+  return {colors, addColor}
+}
 
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState(
     "blue"
   );
+  const {colors, addColor} = useColors();
+  
   return (
     <>
-      <ColorForm onNewColor={newColor => Alert.alert(`TODO: add color ${newColor}`)}
-      />
+      <ColorForm onNewColor={addColor}/>
       <FlatList style={[styles.container, { backgroundColor }]}
-        data={defaultColors}
+        data={colors}
         renderItem={({ item }) => {
           return (
             <ColorButton key={item.id} backgroundColor={item.color} onPress={setBackgroundColor} />
